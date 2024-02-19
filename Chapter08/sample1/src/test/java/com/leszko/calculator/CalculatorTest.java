dd0b335bdc0d75bc1f84f151a393ad117d4530db
Raul Gomez <vmarroquin.dev@gmail.com>
2024-02-19 18:50:55 -0500

    @test made updated to be a comment

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,10 @@
 package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-// class for testing calculator
+
 public class CalculatorTest {
- 	//create a instance 
      private Calculator calculator = new Calculator();
-	//run a quick test
-     @Test
+     //     @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));
      }
