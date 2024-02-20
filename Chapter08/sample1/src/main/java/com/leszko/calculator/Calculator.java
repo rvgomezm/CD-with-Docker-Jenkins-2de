412d203b16da38ded0ca8fca29e6ed75cfa65de5
rvgomezm <158293313+rvgomezm@users.noreply.github.com>
2024-02-19 19:21:32 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,10 +2,8 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
-@Service
 /**
- * comment
+ * @Service
  *
  */
 public class Calculator {
