f25d987843129cbc760f67a45e7304658e401768
rvgomezm <158293313+rvgomezm@users.noreply.github.com>
2024-02-19 19:13:34 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-@Service
+// @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
